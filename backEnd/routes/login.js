f429d12968acf7c2fa6a46c256767362452cf339
sampath <sampathbojjaopensrcproject@gmail.com>
2025-12-31 07:23:37 +0530

    pending task of login

--- backEnd/routes/login.js
+++ backEnd/routes/login.js
@@ -8,15 +8,19 @@ const jwt = require('jsonwebtoken');
 router.post("/", async (req, res) => { // this is /login
   try {
     const { email, password } = req.body;
+
    
     
     const user = await jobSeekerModel.findOne({ email });
+    console.log("this is the user data"+ password);
     
     if (!user) {
       return res.status(401).json({ message: "Invalid email" });
     }
 
+    console.log("this is from the post thing")
     const isMatch = await bcrypt.compare(password, user.profilePassword);
+   
    
     if (!isMatch) {
       return res.status(401).json({ message: "Invalid password" });
